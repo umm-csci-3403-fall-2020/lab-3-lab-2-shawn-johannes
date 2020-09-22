a4659cc7fe91dd4050f942bcd1899406b3969bff
Shawn <reute078@morris.umn.edu>
2020-09-22 11:05:26 -0500

    Create function mergesort_Range
    Implemented the function mergesort_Range very close to the Java model that was given to us.

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,7 +1,30 @@
 #include "mergesort.h"
 
-void mergesort(int, int*) {
+
+
+
+
+
+void mergesort_Range(int, *values, int, startIndex, int, endIndex ) {
+  int rangeSize = endIndex - startIndex;
+  if (needsSorting(rangeSize)) {
+    int midPoint = (startIndex + endInde:x)/2;
+    mergesort_Range(values, startIndex, midPoint);
+    mergesort_Range(values, midPoint, endIndex);
+    merge_Ranges(int, *values, int, startIndex, int, midPoint, int, endIndex);
+  }
+}
+
+
+void mergesort(int, *values) {
   // This obviously doesn't actually do any *sorting*, so there's
   // certainly work still to be done.
   return;
 }
+
+
+
+
+
+
+
