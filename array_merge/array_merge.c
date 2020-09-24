d70bb8382db60a6d24fb293fdea5300edcbb2530
Johannes Martinez <mart4695@morris.umn.edu>
2020-09-24 17:26:50 -0500

    Modify is_unique to count new values (only once)

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -3,14 +3,17 @@
 #include "array_merge.h"
 
  int* is_unique(int num_arrays, int* sizes, int** values, int* binaryArray) {
+  int unique_values = 0;
   for (int outerArrayIndex = 0; outerArrayIndex < num_arrays; outerArrayIndex++) {
     for (int innerArrayIndex = 0; innerArrayIndex < sizes[outerArrayIndex]; innerArrayIndex++) {
        int currentValue = values[outerArrayIndex][innerArrayIndex];
        if (binaryArray[currentValue] == 0) {
           binaryArray[currentValue] = 1;
+          unique_values++;
        }
     }
   }
+  binaryArray[10] = unique_values;
   return binaryArray;
 }
 
