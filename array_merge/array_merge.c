ad99faa50cbff036e1b3f109c5c28e0086923615
Johannes Martinez <mart4695@morris.umn.edu>
2020-09-24 17:13:29 -0500

    Write is_unique function which registers a single time an occurrence of a number in an array

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,6 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "array_merge.h"
 
-int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
-  return sizes;
+ int* is_unique(int num_arrays, int* sizes, int** values, int* binaryArray) {
+  for (int outerArrayIndex = 0; outerArrayIndex < num_arrays; outerArrayIndex++) {
+    for (int innerArrayIndex = 0; innerArrayIndex < sizes[outerArrayIndex]; innerArrayIndex++) {
+       int currentValue = values[outerArrayIndex][innerArrayIndex];
+       if (binaryArray[currentValue] == 0) {
+          binaryArray[currentValue] = 1;
+       }
+    }
+  }
+  return binaryArray;
 }
+
+
