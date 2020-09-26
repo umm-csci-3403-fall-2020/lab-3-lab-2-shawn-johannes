d835790abf5691066a68c0b9b0af0f1994d7afcf
Johannes Martinez <mart4695@morris.umn.edu>
2020-09-25 23:02:53 -0500

    Write findGreatestValue function (details in description)
    The findGreatestValue function returns the greatest number in the values array. This allows us to know the input range [0-greatestValue] which is needed for operating on the binaryArray.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -2,9 +2,7 @@
 #include <stdio.h>
 #include "array_merge.h"
 
- 
-
- int* is_unique(int num_arrays, int* sizes, int** values, int* binaryArray) {
+ int* is_unique(int num_arrays, int* sizes, int** values, int* binaryArray, int greatestValue) {
   int unique_values = 0;
   for (int outerArrayIndex = 0; outerArrayIndex < num_arrays; outerArrayIndex++) {
     for (int innerArrayIndex = 0; innerArrayIndex < sizes[outerArrayIndex]; innerArrayIndex++) {
@@ -15,19 +13,36 @@
        }
     }
   }
-  binaryArray[10] = unique_values; 
+  binaryArray[greatestValue] = unique_values;
   return binaryArray;
 }
 
+int findGreatestValue(int num_arrays, int* sizes, int** values) {
+  int greatestValue = 0;
+   for (int outerArrayIndex = 0; outerArrayIndex < num_arrays; outerArrayIndex++) {
+    for (int innerArrayIndex = 0; innerArrayIndex < sizes[outerArrayIndex]; innerArrayIndex++) {
+       int currentValue = values[outerArrayIndex][innerArrayIndex];
+       if (currentValue > greatestValue) {
+         greatestValue = currentValue;
+       }
+    }
+  }
+  return greatestValue + 1;
+}
+
 int* array_merge(int num_arrays, int* sizes, int** values) {
+  // Find greatest value in range 
+  int greatestValue = findGreatestValue(num_arrays, sizes, values);
+
+  // Create a temporary array for recording unique values (see is_unique)
   int* binaryArray;
-  binaryArray = (int*) calloc(11, sizeof(int));
-  binaryArray = is_unique(num_arrays, sizes, values, binaryArray);
-  int unique_values = binaryArray[10];
+  binaryArray = (int*) calloc(greatestValue + 1, sizeof(int));
+  binaryArray = is_unique(num_arrays, sizes, values, binaryArray, greatestValue);
+  int unique_values = binaryArray[greatestValue];
   int* mergedArray;
   mergedArray = (int*) calloc(unique_values + 1, sizeof(int));
   int mergeArrayIndex = 1;
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < greatestValue; i++) {
     if(binaryArray[i] == 1) {
       mergedArray[mergeArrayIndex] = i;
       mergeArrayIndex++;
