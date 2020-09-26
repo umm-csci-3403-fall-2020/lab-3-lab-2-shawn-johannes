1961fe7d1d8f7b3ffab4b2a92e1f54dce75b868e
Shawn <reute078@morris.umn.edu>
2020-09-25 22:32:14 -0500

    Fix errors with array_merge.c
    Fixed the error invalid read size of 4 we fixed this by finding out that
    we did not create enough space for our mergedArray to fit the unique_values and
    the number of unique_values that were in the array

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include "array_merge.h"
 
+ 
+
  int* is_unique(int num_arrays, int* sizes, int** values, int* binaryArray) {
   int unique_values = 0;
   for (int outerArrayIndex = 0; outerArrayIndex < num_arrays; outerArrayIndex++) {
@@ -13,7 +15,7 @@
        }
     }
   }
-  binaryArray[10] = unique_values;
+  binaryArray[10] = unique_values; 
   return binaryArray;
 }
 
@@ -23,8 +25,7 @@ int* array_merge(int num_arrays, int* sizes, int** values) {
   binaryArray = is_unique(num_arrays, sizes, values, binaryArray);
   int unique_values = binaryArray[10];
   int* mergedArray;
-  // int mergedArray[unique_values + 1];
-   mergedArray = (int*) calloc(unique_values, sizeof(int));
+  mergedArray = (int*) calloc(unique_values + 1, sizeof(int));
   int mergeArrayIndex = 1;
   for (int i = 0; i < 10; i++) {
     if(binaryArray[i] == 1) {
